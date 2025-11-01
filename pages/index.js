fa0c1ca721e43e46c3580beb2fb7a36921301110
Marcelo Cruz <84430175+mfugissecruz@users.noreply.github.com>
2025-11-01 04:47:40 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Entre coroas e protocolos, você é a única regra que eu nunca quebraria.</h1>
+  return (
+    <h1>
+      Entre coroas e protocolos, você é a única regra que eu nunca quebraria.
+    </h1>
+  );
 }
 
 export default Home;
